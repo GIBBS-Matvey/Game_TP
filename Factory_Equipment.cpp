b50008a634cf0e5ba90840f7d12c8c5b251c7243
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-04-10 02:24:29 +0200

    Create Factory_Equipment.cpp

---
+++ Factory_Equipment.cpp
@@ -0,0 +1,23 @@
+#ifndef UNIT_TYPE_H
+#define UNIT_TYPE_H
+
+void Spear::getWeight() {
+    this->weight = SPEAR_W;
+}
+void Spear::getDamage() {
+    this->damage = SPEAR_D;
+}
+void Sword::getWeight() {
+    this->weight = SWORD_W;
+}
+void Sword::getDamage() {
+    this->damage = SWORD_D;
+}
+void Stick::getWeight() {
+    this->weight = STICK_W;
+}
+void Stick::getDamage() {
+    this->damage = STICK_D;
+}
+
+#endif //UNIT_TYPE_H
