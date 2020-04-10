42b4f089ea49eaeab9ef05139b5f1f68145ef074
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-04-10 02:10:09 +0200

    Create Factory_Equipment.h

---
+++ Factory_Equipment.h
@@ -0,0 +1,18 @@
+include "Abstract_Factory_Equipment.h"
+#ifndef UNIT_TYPE_H
+#define UNIT_TYPE_H
+
+class Spear : public Equipment {
+    void getWeight() override;
+    void getDamage() override;
+};
+class Sword : public Equipment {
+    void getWeight() override;
+    void getDamage() override;
+};
+class Stick : public Equipment {
+    void getWeight() override;
+    void getDamage() override;
+};
+
+#endif //UNIT_TYPE_H
