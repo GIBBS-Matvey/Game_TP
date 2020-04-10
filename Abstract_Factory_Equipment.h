ceb07bc62cb3c98c81c604a352c9332213a8cc6d
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-04-10 02:05:53 +0200

    Create Abstract_Factory_Equipment.h

---
+++ Abstract_Factory_Equipment.h
@@ -0,0 +1,16 @@
+#ifndef UNIT_TYPE_H
+#define UNIT_TYPE_H
+
+enum WEIGHT{SPEAR_W = 50, SWORD_W = 35, STICK_W = 10};
+enum DAMAGE{SPEAR_D = 20, SWORD_D = 15, STICK_D = 5};
+
+///Abstract_Equipment_class
+
+class Equipment {
+protected:
+    int weight;
+    int damage;
+public:
+    virtual void getWeight() = 0;
+    virtual void getDamage() = 0;
+};
