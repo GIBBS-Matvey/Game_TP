8ef004bec15f6af8094e825268daad78db037d00
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-03 01:17:33 +0200

    Create geometry.h

---
+++ geometry.h
@@ -0,0 +1,26 @@
+
+
+///..........geometryClasses...........///
+
+
+class Point {
+private:
+    int64_t x;
+    int64_t y;
+public:
+    Point(const int64_t &x, const int64_t &y) : x(x), y(y) {}
+    Point(const Point &point) = default;
+    Point& operator=(const Point &point) = default;
+    friend class Vector;
+};
+
+class Vector {
+private:
+    Point p1;
+    Point p2;
+public:
+    Vector(const Point &p1, const Point &p2) : p1(p1), p2(p2) {}
+    int getLength() const {
+        return pow(pow((p2.x - p1.x), 2) + pow((p2.y - p1.y), 2), 0.5);
+    }
+};
