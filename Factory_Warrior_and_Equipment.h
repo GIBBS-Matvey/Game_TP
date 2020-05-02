cb6a0355001482e59dd97115fd6795b45749cb28
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-03 01:21:52 +0200

    Update and rename Factory_Warrior.h to Factory_Warrior_and_Equipment.h

---
+++ Factory_Warrior_and_Equipment.h
@@ -0,0 +1,62 @@
+#include "Abstract_Factory_Warrior.h"
+#ifndef UNIT_TYPE_H
+#define UNIT_TYPE_H
+
+///..............Warrior_Class.h................///
+
+class Horse_Warrior : public Warrior {
+private:
+    class Spear : public Equipment {
+    public:
+        Spear() : Equipment(SPEAR_W, SPEAR_D) {}
+    };
+public:
+    Horse_Warrior();
+    bool isAlive() override {return alive;}
+    std::string getPhrase() override {return phrase;}
+    virtual int getForceToll() override {return force_toll;}
+    int getType() override {return type;}
+    int getCost() override {return cost;}
+    int getForce() override {return force;}
+    int getHealth() override {return health;}
+};
+
+
+
+class Infantry_Warrior : public Warrior {
+private:
+    class Sword : public Equipment {
+    public:
+        Sword() : Equipment(SWORD_W, SWORD_D) {}
+    };
+public:
+    Infantry_Warrior();
+    std::string getPhrase() override {return phrase;}
+    int getForceToll() override {return force_toll;}
+    bool isAlive() override {return alive;}
+    int getType() override {return type;}
+    int getCost() override {return cost;}
+    int getForce() override {return force;}
+    int getHealth() override {return health;}
+};
+
+
+class Ninja_Warrior : public Warrior {
+private:
+    class Stick : public Equipment {
+    public:
+        Stick() : Equipment(STICK_W, STICK_D) {}
+    };
+public:
+    Ninja_Warrior();
+    bool isAlive() override {return alive;}
+    std::string getPhrase() override {return phrase;}
+    int getForceToll() override {return force_toll;}
+    int getType() override {return type;}
+    int getCost() override {return cost;}
+    int getForce() override {return force;}
+    int getHealth() override {return health;}
+};
+
+
+#endif //UNIT_TYPE_H
