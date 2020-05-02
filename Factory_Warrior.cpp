984b0901a87eb487b1e5aa01016374564a5808ec
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-02 19:22:19 +0200

    Update Factory_Warrior.cpp

--- Factory_Warrior.cpp
+++ Factory_Warrior.cpp
@@ -1,60 +1,68 @@
 #include "Factory_Warrior.h"
 #include "Factory_Equipment.h"
  
-void Horse_Warrior::getType() {
-    this->type = HORSE_WARRIOR;
-}
-void Horse_Warrior::getHealth() {
-    this->health = HORSE_WARRIOR_H;
-}
-void Horse_Warrior::getCost() {
-    this->cost = HORSE_WARRIOR_C;
-}
-void Horse_Warrior::getForce() {
-    this->force = HORSE_WARRIOR_F;
-}
-void Horse_Warrior::usingWeapon(Warrior &enemy) {
-    Spear spear;
-    spear.getDamage();
-    spear.getWeight();
-    enemy.health -= spear.damage;
+void Warrior::usingWeapon(Warrior &enemy) {
+    if (this->force <= 0) {
+        this->health = 0;
+    } else {
+        enemy.health -= this->weapon->damage;
+        enemy.usingWeapon(*this);
+    }
 }
 
-void Infantry_Warrior::getType() {
-    this->type = INFANTRY_WARRIOR;
-}
-void Infantry_Warrior::getHealth() {
-    this->health = INFANTRY_WARRIOR_H;
-}
-void Infantry_Warrior::getCost() {
-    this->cost = INFANTRY_WARRIOR_C;
-}
-void Infantry_Warrior::getForce() {
-    this->force = INFANTRY_WARRIOR_F;
-}
-void Infantry_Warrior::usingWeapon(Warrior &enemy) {
-    Sword sword;
-    sword.getDamage();
-    sword.getWeight();
-    enemy.health -= sword.damage;
-}
- 
-void Ninja_Warrior::getType() {
-    this->type = NINJA_WARRIOR;
-}
-void Ninja_Warrior::getHealth() {
-    this->health = NINJA_WARRIOR_H;
-}
-void Ninja_Warrior::getCost() {
-    this->cost = NINJA_WARRIOR_C;
-}
-void Ninja_Warrior::getForce() {
-    this->force = NINJA_WARRIOR_F;
-}
-void Ninja_Warrior::usingWeapon(Warrior &enemy) {
-    Stick stick;
-    steak.getDamage();
-    steak.getWeight();
-    enemy.health -= sword.damage;
+
+Horse_Warrior::Horse_Warrior() {
+    phrase = "I'm ready to fight!";
+    type = HORSE_WARRIOR;
+    health = HORSE_WARRIOR_H;
+    cost = HORSE_WARRIOR_C;
+    force = HORSE_WARRIOR_F;
+    this->weapon = new Spear();
 }
 
+void Horse_Warrior::move(const Point &new_point) {
+    if (this->force <= 0) {
+        this->health = 0;
+    } else {
+        location = new_point;
+        this->force -= 30;
+    }
+}
+
+Infantry_Warrior::Infantry_Warrior() {
+    phrase = "Let's go!";
+    type = INFANTRY_WARRIOR;
+    health = INFANTRY_WARRIOR_H;
+    cost = INFANTRY_WARRIOR_C;
+    force = INFANTRY_WARRIOR_F;
+    this->weapon = new Sword();
+}
+
+void Infantry_Warrior::move(const Point &new_point) {
+    if (this->force <= 0) {
+        this->health = 0;
+    } else {
+        location = new_point;
+        this->force -= 200;
+    }
+}
+
+Ninja_Warrior::Ninja_Warrior() {
+    phrase = "I'll kill you!";
+    type = NINJA_WARRIOR;
+    health = NINJA_WARRIOR_H;
+    cost = NINJA_WARRIOR_C;
+    force = NINJA_WARRIOR_F;
+    this->weapon = new Stick();
+}
+
+void Ninja_Warrior::move(const Point &new_point) {
+    if (this->force <= 0) {
+        this->health = 0;
+    } else {
+        location = new_point;
+        this->force -= 80;
+    }
+}
+
+#endif//UNIT_TYPE_H
