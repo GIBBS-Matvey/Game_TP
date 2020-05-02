fc9dae5feaf7647ceebe350278970f2dcad1c662
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-02 18:53:31 +0200

    Update Abstract_Factory_Warrior.h

--- Abstract_Factory_Warrior.h
+++ Abstract_Factory_Warrior.h
@@ -1,27 +1,34 @@
-#ifndef UNIT_TYPE_H
-#define UNIT_TYPE_H
+#ifndef Abstract_Factory_Warrior_H
+#define Abstract_Factory_Warrior_H
+#include <string>
+#include <Abstract_Factory_Equipment.h>
 
 enum TYPE{HORSE_WARRIOR = 1, INFANTRY_WARRIOR, NINJA_WARRIOR};
 enum HEALTH{HORSE_WARRIOR_H = 100, INFANTRY_WARRIOR_H = 200, NINJA_WARRIOR_H = 300};
 enum COST{HORSE_WARRIOR_C = 1000, INFANTRY_WARRIOR_C = 800, NINJA_WARRIOR_C = 900};
 enum FORCE{HORSE_WARRIOR_F = 2000, INFANTRY_WARRIOR_F = 1200, NINJA_WARRIOR_F = 1600};
-enum SIDE{BAD, GOOD};
 
 ///Abstract_Warrior_Class
 
 class Warrior {
 protected:
-    int health;
+    Point location = {0, 0};
+    std::string phrase;
+    int type;
     int cost;
     int force;
-    int side;
+    int health;
+    Equipment* weapon;
 public:
-    virtual void getType() = 0;
-    virtual void getHealth() = 0;
-    virtual void getCost() = 0;
-    virtual void getForce() = 0;
-    virtual void getSide() = 0;
-    virtual void usingWeapon(Warrior &enemy) = 0;
+    virtual void usingWeapon(Warrior &enemy);
+    virtual void move(const Point &new_point) = 0;
+    friend class Equipment;
+    friend class Horse_Warrior;
+    friend class Infantry_Warrior;
+    friend class Ninja_Warrior;
+    friend class Spear;
+    friend class Sword;
+    friend class Stick;
 };
 
-#endif //UNIT_TYPE_H
+#endif //Abstract_Factory_Warrior_H
