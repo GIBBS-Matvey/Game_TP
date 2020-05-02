14053e3e227b23d41c3259679dd3b725d896a9d6
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-02 18:50:42 +0200

    Update Abstract_Factory_Equipment.h

--- Abstract_Factory_Equipment.h
+++ Abstract_Factory_Equipment.h
@@ -1,8 +1,18 @@
-#ifndef UNIT_TYPE_H
-#define UNIT_TYPE_H
+#ifndef Abstract_Factory_Equipment_H
+#define Abstract_Factory_Equipment_H
 
 enum WEIGHT{SPEAR_W = 50, SWORD_W = 35, STICK_W = 10};
 enum DAMAGE{SPEAR_D = 20, SWORD_D = 15, STICK_D = 5};
+
+class Equipment;
+class Warrior;
+class Point;
+class Spear;
+class Sword;
+class Stick;
+class Horse_Warrior;
+class Infantry_Warrior;
+class Ninja_Warrior;
 
 ///Abstract_Equipment_class
 
@@ -11,8 +21,23 @@ protected:
     int weight;
     int damage;
 public:
-    virtual void getWeight() = 0;
-    virtual void getDamage() = 0;
+    friend class Warrior;
+    friend class Horse_Warrior;
+    friend class Infantry_Warrior;
+    friend class Ninja_Warrior;
+    friend class Spear;
+    friend class Sword;
+    friend class Stick;
 };
 
-#endif //UNIT_TYPE_H
+class Point {
+private:
+    int64_t x;
+    int64_t y;
+public:
+    Point(const int64_t &x, const int64_t &y) : x(x), y(y) {}
+    Point(const Point &point) = default;
+    Point& operator=(const Point &point) = default;
+};
+
+#endif //Abstract_Factory_Equipment_H
