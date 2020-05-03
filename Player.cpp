d497ef4d503e1597c2bf84e5fb03b08677a35bdd
Matvey <62177479+GIBBS-Matvey@users.noreply.github.com>
2020-05-03 13:41:10 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,34 +1,29 @@
-#include <iostream>
-#include <vector>
-#include "Factory_Warrior_and_Equipment.h"
-
 class Player {
 private:
     int64_t money;
     std::vector<Warrior*> army;
+    int64_t armyCost = 0;
 public:
-    Player(const int64_t &money) : money(money) {}
-    
+    explicit Player(const int64_t &money) : money(money) {}
+
     void BuyWarrior(Warrior *warrior_ptr) {
         if (money >= warrior_ptr->getCost()) {
             army.emplace_back(warrior_ptr);
             money -= army.back()->getCost();
+            armyCost += warrior_ptr->getCost();
         } else {
             std::cout << "Not enough money" << std::endl;
         }
     }
-    
     int64_t GetArmyCost() {
-        int64_t sum(0);
-        for (Warrior *w_ptr : army) {
-            sum += w_ptr->getCost();
-        }
-        return sum;
+        return armyCost;
     }
-    
     void PrintArmy() {
         for (Warrior *w_ptr : army) {
-            std::cout << w_ptr->getType() << ' ';
+            std::cout << "Warrior_type is "<< w_ptr->getType() << ' ' << std::endl;
+            std::cout << "Health is " << w_ptr->getHealth() << ' ' << std::endl;
+            std::cout << "Force is " << w_ptr->getForce() << ' ' << std::endl;
+            std::cout << std::endl;
         }
         std::cout << std::endl;
     }
